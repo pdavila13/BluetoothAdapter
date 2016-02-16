4830c387afac0e0eaec79fbefb95651214a1cbe5
Paolo Davila <pdavila1313@gmail.com>
2016-02-16 19:26:09 +0100

    Add check for Bluetooth hardware

--- app/src/main/java/com/iesebre/dam2/paolo/bluetoothadapter/MainActivity.java
+++ app/src/main/java/com/iesebre/dam2/paolo/bluetoothadapter/MainActivity.java
@@ -1,7 +1,11 @@
 package com.iesebre.dam2.paolo.bluetoothadapter;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Bluetooth is Available!s", Toast.LENGTH_LONG).show();
+        }
     }
 }
