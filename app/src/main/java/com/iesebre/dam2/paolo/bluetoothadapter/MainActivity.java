3cb7b6f078ca167001210648905607f85565797b
Paolo Davila <pdavila1313@gmail.com>
2016-02-16 20:03:40 +0100

    Added BroadcastReceiver and Register the BroadcastReceiver with Register for broadcasts when discovery has finished

--- app/src/main/java/com/iesebre/dam2/paolo/bluetoothadapter/MainActivity.java
+++ app/src/main/java/com/iesebre/dam2/paolo/bluetoothadapter/MainActivity.java
@@ -3,7 +3,10 @@ package com.iesebre.dam2.paolo.bluetoothadapter;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ActionMode;
@@ -20,6 +23,20 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private BluetoothAdapter mBluetoothAdapter;
 
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                Toast.makeText(context, device.getName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Bluetooth is Available!", Toast.LENGTH_LONG).show();
         }
+
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
+        // Register for broadcasts when discovery has finished
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filter);
     }
 
     @Override
